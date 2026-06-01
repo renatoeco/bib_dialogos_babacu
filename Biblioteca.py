09139ad1da52ded604e6fd03f3d91219188a60e0
BernardoISPN <bernardo@ispn.org.br>
2026-06-01 10:19:40 -0300

    Não é mais necessário fazer login para acessar a biblioteca do babaçu. Login fica na sidebar da página "Biblioteca".

--- Biblioteca.py
+++ Biblioteca.py
@@ -7,6 +7,11 @@ import tempfile
 import json
 from funcoes_auxiliares import conectar_mongo_dialogos_babacu, barra_de_logos  
 import re
+import bcrypt
+import time
+import random
+import smtplib
+from email.mime.text import MIMEText
 
 
 # --------------------------------------------------------------
@@ -32,6 +37,7 @@ pessoas = db["pessoas"]
 organizacoes = db["organizacoes"]
 projetos = db["projetos"]
 pesquisas = db["pesquisas"]
+colaboradores = db["pessoas"]
 
 # -------------------------------
 
@@ -67,11 +73,282 @@ def authenticate_drive():
 
 
 
-# -------------------------------
+def encontrar_usuario_por_email(email_busca):
+    usuario = colaboradores.find_one({"e_mail": email_busca})
+
+    if usuario:
+        return usuario.get("nome_completo"), usuario
+
+    return None, None
 
 
+def enviar_email(destinatario, codigo):
 
+    remetente = st.secrets["senhas_email"]["endereco_email"]
+    senha = st.secrets["senhas_email"]["senha_email"]
 
+    assunto = "Código Para Redefinição de Senha - Biblioteca Diálogos do Babaçu"
+
+    corpo = f"""
+    <html>
+        <body>
+            <p style='font-size: 1.5em;'>
+                Seu código para redefinição é:
+                <strong>{codigo}</strong>
+            </p>
+        </body>
+    </html>
+    """
+
+    msg = MIMEText(corpo, "html", "utf-8")
+
+    msg["Subject"] = assunto
+    msg["From"] = remetente
+    msg["To"] = destinatario
+
+    try:
+
+        with smtplib.SMTP_SSL(
+            "smtp.gmail.com",
+            465
+        ) as server:
+
+            server.login(remetente, senha)
+
+            server.sendmail(
+                remetente,
+                destinatario,
+                msg.as_string()
+            )
+
+        return True
+
+    except Exception as e:
+
+        st.error(f"Erro ao enviar e-mail: {e}")
+
+        return False
+
+@st.dialog("Recuperação de Senha")
+def recuperar_senha_dialog():
+    st.session_state.setdefault("codigo_enviado", False)
+    st.session_state.setdefault("codigo_verificacao", "")
+    st.session_state.setdefault("email_verificado", "")
+    st.session_state.setdefault("codigo_validado", False)
+
+    conteudo_dialogo = st.empty()
+
+    # Etapa 1: Entrada do e-mail
+    if not st.session_state.codigo_enviado:
+        with conteudo_dialogo.form(key="recover_password_form", border=False):
+            # Preenche automaticamente com email da sessão (se houver)
+            email_default = st.session_state.get("email_para_recuperar", "")
+            email = st.text_input("Digite seu e-mail:", value=email_default)
+
+            if st.form_submit_button("Enviar código de verificação"):
+                if email:
+                    nome, verificar_colaboradores = encontrar_usuario_por_email(email)
+                    if verificar_colaboradores:
+
+                        if verificar_colaboradores.get("status", "").lower() != "ativo":
+                            st.error("Usuário inativo. Entre em contato com o renato@ispn.org.br.")
+                            return
+                        
+                        codigo = str(random.randint(100, 999))  # Gera um código aleatório
+                        with st.spinner("Enviando código de verificação..."):
+                            if enviar_email(email, codigo):  # Envia o código por e-mail
+                                st.session_state.codigo_verificacao = codigo
+                                st.session_state.codigo_enviado = True
+                                st.session_state.email_verificado = email
+                                st.session_state["nome"] = nome
+                                st.session_state["permissao"] = verificar_colaboradores["permissao"]
+                                
+                                st.success(f"Código enviado para {email}.")
+                            else:
+                                st.error("Erro ao enviar o e-mail. Tente novamente.")
+                    else:
+                        st.error("E-mail não encontrado. Tente novamente.")
+                else:
+                    st.error("Por favor, insira um e-mail.")
+
+    # --- Etapa 2: Verificação do código recebido ---
+    if st.session_state.codigo_enviado and not st.session_state.codigo_validado:
+        with conteudo_dialogo.form(key="codigo_verificacao_form", border=False):
+            st.subheader("Código de verificação")
+            email_mask = st.session_state.email_verificado.replace("@", "​@")  # Máscara leve no e-mail
+            st.info(f"Um código foi enviado para: **{email_mask}**")
+
+            codigo_input = st.text_input("Informe o código recebido por e-mail", placeholder="000")
+            if st.form_submit_button("Verificar"):
+                if codigo_input == st.session_state.codigo_verificacao:
+                    sucesso = st.success("Código verificado com sucesso!")
+                    time.sleep(2)
+                    sucesso.empty()
+                    st.session_state.codigo_validado = True
+                else:
+                    st.error("Código inválido. Tente novamente.")
+
+    # --- Etapa 3: Definição da nova senha ---
+
+    if st.session_state.codigo_validado:
+        with conteudo_dialogo.form("nova_senha_form", border=True):
+            st.markdown("### Defina sua nova senha")
+            nova_senha = st.text_input("Nova senha", type="password")
+            confirmar_senha = st.text_input("Confirme a senha", type="password")
+            enviar_nova_senha = st.form_submit_button("Salvar")
+
+            if enviar_nova_senha:
+                if nova_senha == confirmar_senha and nova_senha.strip():
+                    email = st.session_state.email_verificado
+
+                    usuario = colaboradores.find_one({"e_mail": email})
+
+                    if usuario:
+                        try:
+
+                            hash_senha = bcrypt.hashpw(
+                                nova_senha.encode("utf-8"),
+                                bcrypt.gensalt()
+                            )
+
+                            result = colaboradores.update_one(
+                                {"e_mail": email},
+                                {"$set": {"senha": hash_senha}}
+                            )
+
+                            if result.matched_count > 0:
+
+                                st.success("Senha redefinida com sucesso!")
+
+                                for key in [
+                                    "codigo_enviado",
+                                    "codigo_verificacao",
+                                    "email_verificado",
+                                    "codigo_validado"
+                                ]:
+                                    st.session_state.pop(key, None)
+
+                                st.session_state.logged_in = True
+
+                                time.sleep(2)
+
+                                st.rerun()
+
+                            else:
+                                st.error(
+                                    "Erro ao redefinir a senha. Tente novamente."
+                                )
+
+                        except Exception as e:
+                            st.error(f"Erro ao atualizar a senha: {e}")
+
+                    else:
+                        st.error(
+                            "Nenhum usuário encontrado com esse e-mail."
+                        )
+
+                else:
+                    st.error(
+                        "As senhas não coincidem ou estão vazias."
+                    )
+ 
+                
+def login_sidebar():
+
+    with st.expander("Área administrativa", expanded=False):
+
+        with st.form("login_sidebar_form", border=False):
+
+            email_input = st.text_input("E-mail")
+
+            password = st.text_input(
+                "Senha",
+                type="password"
+            )
+
+            entrar = st.form_submit_button("Entrar")
+
+            if entrar:
+
+                usuario_encontrado = colaboradores.find_one(
+                    {
+                        "e_mail": {
+                            "$regex": f"^{email_input.strip()}$",
+                            "$options": "i"
+                        }
+                    }
+                )
+
+                st.session_state[
+                    "email_para_recuperar"
+                ] = email_input.strip()
+
+                if not usuario_encontrado:
+                    st.error("E-mail ou senha inválidos.")
+                    return
+
+                if (
+                    usuario_encontrado
+                    .get("status", "")
+                    .lower()
+                    != "ativo"
+                ):
+                    st.error(
+                        "Usuário inativo. "
+                        "Entre em contato com "
+                        "renato@ispn.org.br."
+                    )
+                    return
+
+                senha_hash = usuario_encontrado.get(
+                    "senha",
+                    ""
+                )
+
+                if isinstance(senha_hash, str):
+                    senha_hash = senha_hash.encode("utf-8")
+
+                if bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    senha_hash
+                ):
+
+                    st.session_state["logged_in"] = True
+
+                    st.session_state["nome"] = (
+                        usuario_encontrado.get(
+                            "nome_completo"
+                        )
+                    )
+
+                    st.session_state["permissao"] = (
+                        usuario_encontrado.get(
+                            "permissao"
+                        )
+                    )
+                    
+                    st.success("Login efetuado com sucesso", icon=":material/check:")
+                    time.sleep(2)
+                    st.rerun()
+
+                else:
+
+                    st.error(
+                        "E-mail ou senha inválidos."
+                    )
+
+        st.button(
+            "Esqueci a senha",
+            key="forgot_password_sidebar",
+            type="tertiary",
+            on_click=recuperar_senha_dialog
+        )
+
+        st.caption(
+            'Primeiro acesso? Clique em "Esqueci a senha".'
+        )
+        
+                        
 # --------------------------------------------------------------
 # INTERFACE
 # --------------------------------------------------------------
@@ -83,10 +360,22 @@ st.logo("images/logo dialogos do babacu.png", size="large")
 st.header("Biblioteca Diálogos do Babaçu")
 st.write('')
 
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
 
+if "permissao" not in st.session_state:
+    st.session_state["permissao"] = ""
 
+with st.sidebar:
 
+    if not st.session_state["logged_in"]:
+        login_sidebar()
 
+    else:
+        st.caption(
+            f"Logado como: {st.session_state.get('nome', '')}"
+        )
+        
 
 # --------------------------------------------------------------
 # Listagem dos arquivos
